c85cfdad6d4b9fff795abd9d86d28f7fd44f809b
Daniel Noble <daniel.noble@anu.edu.au>
2023-11-24 08:20:29 +1100

    Adding model script

---
+++ R/model.R
@@ -0,0 +1,10 @@
+### ---------------------------------------------------------------  ###
+## Microcimate and Ectotherm model 
+### ---------------------------------------------------------------  ###
+
+# Load packages
+	install.packages("pacman")
+	pacman::p_load(tidyverse, devtools)
+	devtools::install_github('mrke/NicheMapR')
+
+# 
